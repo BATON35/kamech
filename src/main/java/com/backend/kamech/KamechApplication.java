b76bf568b2b2664f76269e2a09c4d906f9b88a1c
adamKubala <adam.kubala90@gmail.com>
2021-03-31 21:28:04 +0200

    Initial commit

---
+++ src/main/java/com/backend/kamech/KamechApplication.java
@@ -0,0 +1,13 @@
+package com.backend.kamech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/kamech/KamechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.kamech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
